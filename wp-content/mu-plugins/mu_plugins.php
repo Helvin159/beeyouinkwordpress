e11a6a93dc647958754b57cef2603633e12414d7
Helvin Rymer <helvin159@gmail.com>
2022-11-25 11:45:44 -0500

    adds component scripts, sets up header and footer, about ready for REST API setup

---
+++ wp-content/mu-plugins/mu_plugins.php
@@ -0,0 +1,63 @@
+<?php 
+
+function bee_you_ink_post_types(){
+    register_post_type('hero', array(
+    'show_in_rest' => true,
+    'supports' => array(
+        'title', 'editor', 'excerpt',
+      ),
+    'public' => true,
+    'labels'=>array(
+      'name' => 'Hero',
+      'add_new_item' => 'Add New Hero Slide',
+      'edit_items' => 'Edit Hero Slide',
+      'all_items' => 'All Hero Slides',
+      'singular_name' => 'Hero Slide',
+    ),
+    'menu_icon' => 'dashicons-laptop',
+  ));
+  
+  register_post_type('articles', array(
+      'show_in_rest' => true,
+      'supports' => array(
+        'title', 'editor', 'excerpt',
+      ),
+    'public' => true,
+    'labels'=>array(
+      'name' => 'Articles',
+      'add_new_item' => 'Add New Article',
+      'edit_items' => 'Edit Article',
+      'all_items' => 'All Articles',
+      'singular_name' => 'Article',
+    ),
+    'menu_icon' => 'dashicons-slides',
+  ));
+
+register_post_type('testimonial', array(
+    'public' => true,
+    'labels'=>array(
+      'name' => 'Testimonials',
+      'add_new_item' => 'Add New Testimonial',
+      'edit_items' => 'Edit Testimonial',
+      'all_items' => 'All Testimonials',
+      'singular_name' => 'Testimonial',
+    ),
+    'menu_icon' => 'dashicons-groups',
+    'show_in_rest' => true
+  ));
+  
+  register_post_type('tattoo_work', array(
+      'show_in_rest' => true,
+    'public' => true,
+    'labels'=>array(
+      'name' => 'Tattoos',
+      'add_new_item' => 'Add New Tattoo',
+      'edit_items' => 'Edit Tattoo',
+      'all_items' => 'All Tattoos',
+      'singular_name' => 'Tattoo',
+    ),
+    'menu_icon' => 'dashicons-admin-customizer',
+  ));
+}
+
+add_action('init', 'bee_you_ink_post_types');
--- wp-content/themes/BeeYouInkTwo/index.php
+++ wp-content/themes/BeeYouInkTwo/index.php
@@ -1,23 +1,8 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="/favicon.ico"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <meta name="theme-color" content="#000000"/>
-        <meta name="description" content="Web site created using create-react-app"/>
-        <link rel="apple-touch-icon" href="<?php echo get_theme_file_uri("/logo192.png") ?>" />
-        <link rel="manifest" href="/manifest.json"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
-            <link rel="stylesheet" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
-            <title>Bee You Ink</title>
-            <script defer="defer" src="<?php echo get_theme_file_uri("/static/js/main.166b710b.js") ?>"></script>
-            <link href="<?php echo get_theme_file_uri("/static/css/main.1d2ea062.css") ?>" rel="stylesheet">
-        </head>
-        <body>
-            <noscript>You need to enable JavaScript to run this app.</noscript>
-            <div id="root"></div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        </body>
-</html>
+<?php get_header(); ?>
+    <!-- Components to loop through -- once setup on wordpress -->
+    <div id="hero"></div>
+    <div id="testimonialSlider"></div>
+    <div id="storyText"></div>
+    <div id="pageCollage"></div>
+    <div id="recentArticlesComponent"></div>
+<?php get_footer(); ?>
