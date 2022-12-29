1e92e01eb3434328f4b1111b85f3343a95494022
Helvin Rymer <helvin159@gmail.com>
2022-12-29 09:24:32 -0500

    some updates

--- wp-content/themes/beeyouinktwo-react/functions.php
+++ wp-content/themes/beeyouinktwo-react/functions.php
@@ -13,7 +13,7 @@ function beeyou_files(){
   // ******* CSS FILES ******************
   // ************************************ 
   // My Style
-  wp_enqueue_style('styles', get_theme_file_uri('./static/css/main.140bdb4b.css'));
+  wp_enqueue_style('styles', get_theme_file_uri('./static/css/main.103659b2.css'));
   
   // Bootstrap CSS
   wp_enqueue_style('bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
