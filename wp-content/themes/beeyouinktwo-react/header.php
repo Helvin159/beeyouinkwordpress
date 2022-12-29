aa161874f48cae2b64b8f6ad686e4201527c231c
Helvin Rymer <helvin159@gmail.com>
2022-12-28 23:04:00 -0500

    some updates

--- wp-content/themes/beeyouinktwo-react/header.php
+++ wp-content/themes/beeyouinktwo-react/header.php
@@ -4,6 +4,11 @@
         <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <!-- CSS Files & Head Scripts -->
         <?php wp_head(); ?>
+        <style>
+            .aligncenter{
+                margin:0 auto;
+            }
+        </style>
         </head>
         <body>
             <noscript>You need to enable JavaScript to run this app.</noscript>
